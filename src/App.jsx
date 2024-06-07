44b6979f70ca9d76056f0f776b39fa6c98623348
Jorge_Jimenez <112440747+jorge-ivan-jimenez-reyes@users.noreply.github.com>
2024-06-07 22:30:47 +0000

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jorge Ivan Jimenez",
+  title: "Developer | Student",
+  email: "jorgeivanjimenez27@gmail.com",
+  gitHub: " jorge-ivan-jimenez-reyes",
+  instagram: "_jimenez_jorge_",
+  linkedIn: "Jorge Jimenez",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "jorgejimenez",
+  youTube: "gfreshhh",
 };
 
 const primaryColor = "#4E567E";
